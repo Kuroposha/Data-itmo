8eaa3a94c5dcef3b54c559eb281814128fb00f76
Maria K <35758685+Kuroposha@users.noreply.github.com>
2018-03-16 11:49:58 +0300

    Update command.py

--- lesson-patterns/command.py
+++ lesson-patterns/command.py
@@ -1,31 +1,40 @@
 # Шаблон Command - поведенческий шаблон
 """
 Инкапсулирует запрос как объект
-
 """
 from ABC import ABCMeta, abstractmethod
+
 
 class Command(metaclass=ABCMeta):
     __command = {}
 
     @abstractmethod
-    def execute(self):
+    def _do_execute(self):
         pass
+
+    def execute(self):
+        #Tamplate method - шаблонный метод
+        # Выполняем действия до
+        self._do_execute()# неизвестная часть алгоритма
+        # Выполняем действия после
 
     @staticmethod
     def command(name):
         def decorator(cls):
             if not name:
                 raise RuntimeError
+
             if not issubclass(cls, Command):
                 raise TypeError
 
             Command.__commands[name] = cls
-            return 
+            return cls
+        return decorator
 
     @classmethod
     def get_instance(cls, name, *args, **kwargs):
-        #fabriq
+        """Шаблон фабричный метод (factory method)"""
+
         klass = cls.__command,get(name)
 
         if not klass:
@@ -33,13 +42,22 @@ class Command(metaclass=ABCMeta):
 
         return klass(*args, **kwargs)
 
+
+@Command.command('list')
 class ListCommand(Command):
     def execute(self):
         print('Показать все записи ежедневника')
 
-class Show(Command):
+
+@Command.command('show')
+class ShowCommand(Command):
     def __init__(self, pk):
         self.__pk = pk
 
     def execute(self):
-        print('Показать запись с ID: {}')
+        print('Показать запись с ID: {}'.format(self.__pk))
+
+commands = [ShowCommand(1), ListCommand(), Command.get_instance('show', 100)]
+
+for cmd in commands:
+    cmd.execute()
